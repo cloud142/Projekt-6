6b7908d857f7c228b0789c68d51142d639049881
kingsalle <salma2009@live.dk>
2023-11-26 19:31:40 +0100

    hey

--- js/salma.js
+++ js/salma.js
@@ -1,33 +1,23 @@
-let slideIndex = 1;
+let slideIndex = 0;
 
-function showSlides(n) {
+function showSlides() {
   let i;
   const slides = document.getElementsByClassName("mySlides");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
+  
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
 
-  slides[slideIndex - 1].style.display = "block";
+  slideIndex++;
+  
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }    
+
+  slides[slideIndex - 1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Skift billede hvert 2000 millisekunder (2 sekunder)
 }
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// Kald showSlides funktionen for at starte slideshowet
+showSlides();
 
-// Display the first slide
-showSlides(slideIndex);
-
-// Handle button click event
-document.getElementById("discoverButton").addEventListener("click", () => {
-  // Redirect to the winter collection page or handle as needed
-  alert("Redirecting to Winter Collection!");
-});
